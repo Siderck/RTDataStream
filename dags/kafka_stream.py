8b5c83cc0b4246aade75d128a0a9191e6ffb86de
Siderck <bryanxd@live.cl>
2024-08-01 23:44:34 -0400

    json schema created

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -17,7 +17,19 @@ def get_data():
 
 def format_data(res):
     data = {}
-    data['first_name']
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
 
 
 def stream_data():
